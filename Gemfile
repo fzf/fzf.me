87f5ffa345470cd1e8249407f34fee6bc1848b31
Fletcher Fowler <fletch@fzf.me>
2013-10-07 03:00:16 -0700

    I: initial sinatra commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'compass'
+gem 'haml'
+gem 'sass'
+gem 'sinatra'
+gem 'sinatra-twitter-bootstrap', :require => 'sinatra/twitter-bootstrap'
+
+group :development do
+  gem 'pry'
+  gem 'shotgun'
+  gem 'tux'
+end
---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'compass' #must be loaded before sinatra
+require 'sinatra'
+require 'sinatra/twitter-bootstrap'
+require 'haml'    #must be loaded after sinatra
+require 'pry'
+
+
+# set sinatra's variables
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :views, "views"
+set :public_dir, 'static'
+set :fonts_dir, 'fonts'
+set :images_dir, 'images'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+  register Sinatra::Twitter::Bootstrap::Assets
+end
+
+
+# at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+end
+
+get '/' do
+  haml :index
+end
+
+helpers do
+  def img_tag(name)
+    "<img src='images/#{name}' alt='#{name}' />"
+  end
+
+  def link_to(url,text=url,opts={})
+    attributes = ""
+    opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
+    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+  end
+end
